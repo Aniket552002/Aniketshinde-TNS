891037a0375a4ff6e46d42aa5df058b2375714dc
Aniket552002 <aniketrameshshinde2002@gmail.com>
2024-10-03 17:12:06 +0530

    first commit

---
+++ day_1/src/day_1/firstprogram/firstprogram.java
@@ -0,0 +1,10 @@
+package day_1.firstprogram;
+
+public class firstprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.print("**********dharmo rakshti rakshit:************");
+	}
+
+}
